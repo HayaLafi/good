b5c8e989694e038ef96996c743a93227c8f7efe5
HayaLafi <11haya.hh@gmail.com>
2022-06-14 18:28:20 +0300

    first commit

---
+++ src/main/java/com/example/haya2022/Haya2022Application.java
@@ -0,0 +1,13 @@
+package com.example.haya2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Haya2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Haya2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/haya2022/Haya2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.haya2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Haya2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
